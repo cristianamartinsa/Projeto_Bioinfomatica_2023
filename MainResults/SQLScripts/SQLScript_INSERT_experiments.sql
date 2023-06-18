ea441c04e193925e0b1739c0560461ab0aaf1585
cristianamartinsa <96478330+cristianamartinsa@users.noreply.github.com>
2023-06-18 11:46:37 +0100

    Delete MainResults directory

--- MainResults/SQLScripts/SQLScript_INSERT_experiments.sql
+++
@@ -1,9 +0,0 @@
--- -----------------------------------------------------
--- INSERTS Table `projetobioinf`.`experiments`
--- -----------------------------------------------------
-
-INSERT INTO 
-experiments(experiment_id, description)
-VALUES
-(1, 'BLASTp similarity with viral proteins recognized by the human innate receptors'),
-(2, 'PubMed Search');
